d073c27b7bd937e34f325c9ebf6d568c5235e869
Jonathon Mangan <jtmangac@gmail.com>
2023-04-11 21:52:50 -0400

    functionality for homework 9

--- src/main/java/edu/iu/c322/customerservice/repository/CustomerRepository.java
+++ src/main/java/edu/iu/c322/customerservice/repository/CustomerRepository.java
@@ -4,8 +4,6 @@ import edu.iu.c322.customerservice.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
